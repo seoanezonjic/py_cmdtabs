3fa4456b0854a904b15a1dcfaccd8783acd67b76
seoanezonjic <darklordsone@hotmail.com>
2022-11-09 18:33:41 +0100

    Test_input_parsing finished

--- py_cmdtabs/cmdtabs.py
+++ py_cmdtabs/cmdtabs.py
@@ -1,4 +1,6 @@
 import sys
+import os.path
+from collections import defaultdict
 
 class CmdTabs:
 	def load_input_data(input_path, sep="\t", limit=-1):
@@ -11,5 +13,68 @@ class CmdTabs:
 		input_data_arr = []
 		for line in input_data:
 			#print('---', file=sys.stderr)
+			#print(repr(), file=sys.stderr)
 			input_data_arr.append(line.rstrip().split(sep, limit))
 		return input_data_arr
+
+	def build_pattern(col_filter, keywords):
+	    pattern = {}
+	    if col_filter != None and keywords != None:
+	        keys_per_col = keywords.split('%')
+	        if len(keys_per_col) != len(col_filter): abort('Number of keywords not equal to number of filtering columns') 
+	        i = 0
+	        for col in col_filter:
+	            pattern[col] = keys_per_col[i].split('&')
+	            i += 1
+	    return pattern
+
+	def index_array(array, col_from=0, col_to=1):
+		indexed_array = {}
+		for elements in array:
+			indexed_array[elements[col_from]] = elements[col_to]
+		return indexed_array
+
+	def index_metrics(input_data, attributes):
+		n_attrib = len(attributes)
+		indexed_metrics = defaultdict(lambda: False)
+		metric_names = []
+		for entry in input_data:
+			sample_id = entry.pop(0)
+			sample_attributes = entry[0:n_attrib - 1]
+			metric_name, metric = entry[n_attrib:n_attrib + 2] # +2 due to select the next element and the upper bound is not contained in the subset
+			if metric_name not in metric_names: metric_names.append(metric_name)
+			query = indexed_metrics[sample_id]
+			if not query:
+				indexed_metrics[sample_id] = {metric_name: metric}
+				i = 0
+				for attrib in attributes:
+					indexed_metrics[sample_id][attrib] = sample_attributes[i] 
+					i += 1
+			else:
+				query[metric_name] = metric
+		return metric_names, indexed_metrics
+
+	def parse_column_indices(sep, col_string):
+	    cols = []
+	    for col in col_string.split(sep):
+	    	cols.append(int(col) - 1)
+	    return cols
+
+	def load_and_parse_tags(tags, sep):
+		parsed_tags = []
+		for tag in tags:
+			if os.path.exists(tag):
+				parsed_tags.extend(CmdTabs.load_input_data(tag, sep))
+				break
+			else:
+				parsed_tags.append(tag.split(sep))
+		return [item for sublist in parsed_tags for item in sublist] #Flatten list
+
+	def load_records(input_file, cols, full): # TODO: Convert list to string to be hasheable is a workaround. We must search another way to do this!!!
+		records = {}
+		full_row_of_records = {}
+		for fields in input_file:
+			field = "\t".join([fields[c] for c in cols]) # Lists are not hasheable in Python so we covert them to string
+			records[field] = True
+			if full: full_row_of_records[field] = fields 
+		return list(records.keys()), full_row_of_records
--- test/test_input_parsing.py
+++ test/test_input_parsing.py
@@ -8,61 +8,63 @@ class InputParsingTestCase(unittest.TestCase):
 	def test_build_pattern(self):
 		col_filter = [0, 1, 2]
 		keywords = "key1_col1&key2_col1%key1_col2&key2_col2%key1_col3"
-		pattern_test = build_pattern(col_filter, keywords)
+		pattern_test = CmdTabs.build_pattern(col_filter, keywords)
 		expected_result = {0 : ["key1_col1", "key2_col1"], 1 : ["key1_col2", "key2_col2"], 2 : ["key1_col3"]}
 		self.assertEqual(expected_result, pattern_test)
 
 	def test_parse_column_indices(self):
 		cols_string = "1,3,4,7"
-		col_indx_test = parse_column_indices(",", cols_string)
+		col_indx_test = CmdTabs.parse_column_indices(",", cols_string)
 		expected_result = [0,2,3,6]
 		self.assertEqual(expected_result, col_indx_test)
 
 	def test_load_and_parse_tags_file(self):
 		input_tags = [os.path.join(DATA_TEST_PATH, 'tracker')]
-		tag_test = load_and_parse_tags(input_tags, "\t")
+		tag_test = CmdTabs.load_and_parse_tags(input_tags, "\t")
 		expected_result = ['MERGED_net_no_raw_cpm', 'MERGED', 'no', 'no', 'cpm']
 		self.assertEqual(expected_result, tag_test)
 
 	def test_load_and_parse_tags_stdin(self):
 		input_tags = ['MERGED_net_no_raw_cpm', 'MERGED', 'no', 'no', 'cpm']
-		tag_test = load_and_parse_tags(input_tags, "\t")
+		tag_test = CmdTabs.load_and_parse_tags(input_tags, "\t")
 		expected_result = ['MERGED_net_no_raw_cpm', 'MERGED', 'no', 'no', 'cpm']
 		self.assertEqual(expected_result, tag_test)
 
 	def test_load_records(self):
 		input_table = CmdTabs.load_input_data(os.path.join(DATA_TEST_PATH, 'disease_cluster'))
-		records_test = load_records(input_table, [0], false)
-		expected_result = [['MONDO:0008995'], ['MONDO:0007172'], ['MONDO:0014823'], ['MONDO:0017999'], ['MONDO:0011142'],
-			['MONDO:0009833'], ['MONDO:0009594'], ['MONDO:0010193'], ['MONDO:0012176'], ['MONDO:0013969']], {}
+		records_test = CmdTabs.load_records(input_table, [0], False)
+		expected_result = ['MONDO:0008995', 'MONDO:0007172', 'MONDO:0014823', 'MONDO:0017999', 'MONDO:0011142',
+		'MONDO:0009833', 'MONDO:0009594', 'MONDO:0010193', 'MONDO:0012176', 'MONDO:0013969'], {}
 		self.assertEqual(expected_result, records_test)
 
 	def test_load_records_full(self):
 		input_table = CmdTabs.load_input_data(os.path.join(DATA_TEST_PATH, 'disease_cluster'))
-		records_full_test = load_records(input_table, [0], true)
-		expected_result = [['MONDO:0008995'], ['MONDO:0007172'], ['MONDO:0014823'], ['MONDO:0017999'], ['MONDO:0011142'],
-		['MONDO:0009833'], ['MONDO:0009594'], ['MONDO:0010193'], ['MONDO:0012176'], ['MONDO:0013969']], 
-		{['MONDO:0008995'] : ['MONDO:0008995', '19_ref'], ['MONDO:0007172'] : ['MONDO:0007172', '22_ref'], 
-		['MONDO:0014823'] : ['MONDO:0014823', '25_ref'], ['MONDO:0017999'] : ['MONDO:0017999', '53_ref'],
-		['MONDO:0011142'] : ['MONDO:0011142', '66_ref'], ['MONDO:0009833'] : ['MONDO:0009833', '53_ref'],
-		['MONDO:0009594'] : ['MONDO:0009594', '54_ref'], ['MONDO:0010193'] : ['MONDO:0010193', '54_ref'], 
-		['MONDO:0012176'] : ['MONDO:0012176', '62_ref'], ['MONDO:0013969'] : ['MONDO:0013969', '1189_ref']}
-		self.assertEqual(expected_result, records_full_test)
+		selected_fields, full_records = CmdTabs.load_records(input_table, [0], True)
+		expected_selected_fields = ['MONDO:0008995', 'MONDO:0007172', 'MONDO:0014823', 'MONDO:0017999', 'MONDO:0011142',
+		'MONDO:0009833', 'MONDO:0009594', 'MONDO:0010193', 'MONDO:0012176', 'MONDO:0013969'] 
+		expected_full_records = {'MONDO:0008995' : ['MONDO:0008995', '19_ref'], 'MONDO:0007172' : ['MONDO:0007172', '22_ref'], 
+		'MONDO:0014823' : ['MONDO:0014823', '25_ref'], 'MONDO:0017999' : ['MONDO:0017999', '53_ref'],
+		'MONDO:0011142' : ['MONDO:0011142', '66_ref'], 'MONDO:0009833' : ['MONDO:0009833', '53_ref'],
+		'MONDO:0009594' : ['MONDO:0009594', '54_ref'], 'MONDO:0010193' : ['MONDO:0010193', '54_ref'], 
+		'MONDO:0012176' : ['MONDO:0012176', '62_ref'], 'MONDO:0013969' : ['MONDO:0013969', '1189_ref']}
+		self.assertEqual(expected_selected_fields, selected_fields)
+		self.assertEqual(expected_full_records, full_records)
 
 	def test_index_metrics(self):
 		metric_file = CmdTabs.load_input_data(os.path.join(DATA_TEST_PATH, 'all_metrics'))
 		fixCols = 'sample'
 		attributes = fixCols.split(',')
-		attributes.shift
-		indexed_metrics_test = index_metrics(metric_file, attributes)
-		expected_result = ['initial_total_sequences', 'initial_read_max_length', 'initial_read_min_length', 'initial_%gc'],
-		{'CTL_1_cell' : {'initial_total_sequences' : '11437331.0', 'initial_read_max_length' : '76.0', 
+		attributes.pop(0)
+		metric_names, indexed_metrics = CmdTabs.index_metrics(metric_file, attributes)
+		expected_metric_names = ['initial_total_sequences', 'initial_read_max_length', 'initial_read_min_length', 'initial_%gc']
+		expected_indexed_metrics = {'CTL_1_cell' : {'initial_total_sequences' : '11437331.0', 'initial_read_max_length' : '76.0', 
 		'initial_read_min_length' : '35.0', 'initial_%gc' : '45.0'}, 'CTL_1_exo' : {'initial_total_sequences' : '10668412.0', 
 		'initial_read_max_length' : '76.0', 'initial_read_min_length' : '35.0', 'initial_%gc' : '48.0'}}
-		self.assertEqual(expected_result, indexed_metrics_test)
+		self.assertEqual(expected_metric_names, metric_names)
+		self.assertEqual(expected_indexed_metrics, indexed_metrics)
 
 	def test_index_array(self):
 		input_index = CmdTabs.load_input_data(os.path.join(DATA_TEST_PATH, 'disease_gene'))
-		indexed_test = index_array(input_index)
+		indexed_test = CmdTabs.index_array(input_index)
 		expected_result = {'MONDO:0010193' : 'HGNC:3527', 'MONDO:0008995' : 'HGNC:16873', 'MONDO:0012866' : 'HGNC:21197', 'MONDO:0017999' : 'HGNC:21197', 'MONDO:0011142' : 'HGNC:21144', 'MONDO:0013969' : 'HGNC:21176', 'MONDO:0018053' : 'HGNC:21157'}
 		self.assertEqual(expected_result, indexed_test)
