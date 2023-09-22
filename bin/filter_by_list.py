9cff60ab4860891b21d4571ec754119d44a19575
federedef <federogc98@gmail.com>
2023-09-22 18:48:55 +0200

    adding metrics options in filter_by_list

--- bin/filter_by_list.py
+++ bin/filter_by_list.py
@@ -13,7 +13,9 @@ parser.add_argument("-f", "--files2befiltered", default= None, type = lambda x: 
 parser.add_argument("-c", "--columns2befiltered", default = None, type = lambda	x: [list(map(int,r.split(","))) for r in x.split(";")] ,help="The columns that need to be filtered for each file, separated by semicolons, with each set of columns separated by commas")
 parser.add_argument("-t", "--terms2befiltered", default= None, required=True, help="The PATH to the list of terms to be filtered")
 parser.add_argument("--transposed", dest= "transposed", default= False, action="store_true", help="To perform the operations in rows and not columns")
+parser.add_argument("--prefix", dest= "prefix", default= "filtered_", help="To select which prefix to add in new filterd files")
 parser.add_argument("-o", "--output_path", default=".", help="The name of the output path")
+parser.add_argument("--metrics", default=False, action="store_true", help= "Getting a table with the proportion of lines removed for each file")
 options = parser.parse_args()
 
 ##################### MAIN #######################
@@ -26,6 +28,7 @@ CmdTabs.transposed = options.transposed
 files2befiltered = options.files2befiltered
 columns2befiltered = options.columns2befiltered
 files_columns2befiltered = list(zip(files2befiltered,columns2befiltered))
+loaded_files = CmdTabs.load_several_files(options.files2befiltered)
 
 output_path = options.output_path
 
@@ -33,16 +36,22 @@ file_filteredfile = {}
 for file_columns in files_columns2befiltered:
 	file = file_columns[0]
 	columns = file_columns[1]
-	table = CmdTabs.load_input_data(file)
-	print("ey1")
+	table = loaded_files[file]
 	for column in columns:
-		print("ey1.1")
 		table = CmdTabs.filter_by_whitelist(table, terms2befiltered, column)
-		print("ey2")
 	file_filteredfile[file] = table
-	print("ey3")
 
 
 for file_path, filtered_table in file_filteredfile.items():
 	file_name = os.path.basename(file_path)
-	CmdTabs.write_output_data(filtered_table, output_path=os.path.join(output_path,"filtered_" + file_name))
+	CmdTabs.write_output_data(filtered_table, output_path=os.path.join(output_path, options.prefix + file_name))
+
+if options.metrics:
+	for file2befiltered in options.files2befiltered:
+		filtered_file = file_filteredfile.get(file2befiltered)
+		if filtered_file is None:
+			print(file2befiltered + "\t" + "Not Filtered")
+		else:
+			ratio_lines_removed = round(100*(len(file_filteredfile[file2befiltered])/len(loaded_files[file2befiltered])),2)
+			print(file2befiltered + "\t" + str(ratio_lines_removed))
+
