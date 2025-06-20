e5f24ee626924a3b036842a3a4508af189f22245
TheWildParamecium <perezgarcia_jesus94@hotmail.com>
2025-06-20 11:54:16 +0200

    First draft of cli_example_report. Two methods remain to be added yet

---
+++ tests/cli_example_report/launch.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+. ~soft_bio_267/initializes/init_python
+#python -m venv venv --system-site-packages
+source venv/bin/activate
+#pip install -e ~/dev_py/py_cmdtabs
+#pip install -e ~/dev_py/py_report_html
+
+report_html -t template.txt -d "./tables/*" -s ./subtemplates
+
+
